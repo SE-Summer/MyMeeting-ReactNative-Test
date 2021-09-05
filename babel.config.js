6faeae2e63c44152c7bf18d32a66536d3299b096
Casieee <cen-le@sjtu.edu.cn>
2021-09-05 19:41:14 +0800

    add some tests

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,9 @@ module.exports = {
   presets: [
     'module:metro-react-native-babel-preset', 
   ],
+  env: {
+    "test": {
+      "plugins": ["@babel/plugin-transform-modules-commonjs"]
+    }
+  }
 };
